3767ed6e236cbbbe66e0e6222e7b1554cb39ec0e
REN <0750135960@CBP.DHS.GOV>
2017-06-28 16:12:27 -0400

    Stopping at 2-38

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServerComponent } from '../server/server.component';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.allowNewServer = true;
+      },
+      2000
+    )
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!  Name is ' + this.serverName;
+  }
+
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
+}
